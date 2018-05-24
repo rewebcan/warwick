0d21dce2b9288e5c0560ea11a68c7d5b63ac0c4a
Recep Can <rewebcan@gmail.com>
2018-05-25 00:01:09 +0300

    exported ww class from library, dist not ready to share yet

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,1 @@
-import { Warwick } from "./warwick/warwick";
-
-const ww = new Warwick({ apiKey: 'RGAPI-1ce8a4a1-dda9-46b5-8106-43c2745d3158' });
-ww.region('tr1').account(200653993).matches(
-    (matches: any)=> {
-        console.log(matches)
-    }
-)
+export {Warwick} from "./warwick/warwick"; 
